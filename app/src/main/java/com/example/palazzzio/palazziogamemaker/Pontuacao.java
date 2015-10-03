4a85b3ff5b02800cc8e805554d3be78ea80f849f
Ricardo Palazzio <ricardo.palazzio@gmail.com>
2015-10-03 17:00:10 -0300

    hh

---
+++ app/src/main/java/com/example/palazzzio/palazziogamemaker/Pontuacao.java
@@ -0,0 +1,7 @@
+package com.example.palazzzio.palazziogamemaker;
+
+/**
+ * Created by palazzzio on 03/10/15.
+ */
+public class Pontuacao {
+}
---
+++ app/src/main/java/com/example/palazzzio/palazziogamemaker/Sound.java
@@ -0,0 +1,11 @@
+package com.example.palazzzio.palazziogamemaker;
+
+/**
+ * Created by palazzzio on 03/10/15.
+ */
+public class Sound {
+
+    private Sound sound;
+
+    public S
+}
